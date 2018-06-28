0305ae852676c04938381629abee6e2ef7385fc5
Jake <jake.myshrall@gmail.com>
2018-06-27 22:41:11 -0300

    Adding CLI support through the cement framework; Added setup.py to package cloud-insight

---
+++ setup.py
@@ -0,0 +1,55 @@
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = 'cloud-insight'
+DESCRIPTION = 'Simplifies the tracking of docker container versions,' \
+              ' health and other important information across various platforms.'
+URL = 'https://github.com/noahjohnhay/cloud-insight'
+EMAIL = "noahjohnhay@gmail.com"
+AUTHOR = 'Noah Hay'
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+REQUIRED = [
+    'boto3',
+    'python-consul',
+    'plotly',
+    'cement',
+]
+
+setup(
+    name=NAME,
+    version='0.0.2',
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    entry_points={
+         'console_scripts': ['cloud-insight=packages.cli:main'],
+     },
+    install_requires=REQUIRED,
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+
+)
