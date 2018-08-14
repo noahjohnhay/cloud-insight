09e76b981eab25696ba4a847e2f08849d9229e58
Hay, Noah <nhay@cvent.com>
2018-08-14 18:27:33 -0300

    cli-table, refactoring, etc...

--- packages/executor.py
+++ packages/executor.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python
 
-import logging
 import packages.aws as aws
 import packages.ecs as ecs
 import packages.settings as settings
-import packages.table as table
-import packages.asg as asg
+import packages.output as output
+import sys
 
 
-def aws_caller(ecs_client, ecs_services):
+def aws_caller(app, ecs_client, ecs_services):
 
     # ITERATE THROUGH ECS CLUSTERS
     for ecs_cluster in ecs.list_clusters(ecs_client)['clusterArns']:
 
         # PRINT CLUSTERS
-        print('AWS: Found cluster {0}'.format(
+        app.log.info('AWS: Found cluster {0}'.format(
             aws.parse_arn(ecs_cluster)['resource'])
         )
 
         # ITERATE THROUGH SERVICES IN EACH CLUSTER
-        for ecs_service in ecs.list_services(ecs_client, aws.parse_arn(ecs_cluster)['resource']):
+        for ecs_service in ecs.list_services(app, ecs_client, aws.parse_arn(ecs_cluster)['resource']):
 
             # CREATE SERVICE DICTIONARY
             service = dict()
@@ -31,7 +30,7 @@ def aws_caller(ecs_client, ecs_services):
             service['cluster'] = aws.parse_arn(ecs_cluster)['resource']
 
             # PRINT SERVICES
-            print('AWS: Found service {0}'.format(
+            app.log.info('AWS: Found service {0}'.format(
                 aws.parse_arn(ecs_service)['resource'])
             )
 
@@ -43,7 +42,7 @@ def aws_caller(ecs_client, ecs_services):
             )
 
             # PRINT SERVICE DESCRIPTION
-            print('AWS: Service {0}, Count {1}, Active Task Definition {2}'.format(
+            app.log.info('AWS: Service {0}, Count {1}, Active Task Definition {2}'.format(
                 aws.parse_arn(ecs_service)['resource'],
                 ecs_service_description['services'][0]['desiredCount'],
                 ecs_service_description['services'][0]['taskDefinition'])
@@ -63,103 +62,98 @@ def aws_caller(ecs_client, ecs_services):
             )
 
             # ADD VERSION ITEM TO DICTIONARY
-            service['version'] = ecs_task_description \
-                ['taskDefinition']['containerDefinitions'][0]['image'].split(':', 1)[-1]
+            service['version'] = \
+                ecs_task_description['taskDefinition']['containerDefinitions'][0]['image'].split(':', 1)[-1]
 
             # APPEND DICTIONARY ITEMS TO ARRAY
             ecs_services.append(service)
 
-    return ecs_services
+    sorted_ecs_services = sorted(ecs_services, key=lambda k: k['name'])
+
+    return sorted_ecs_services
 
 
-def execute():
+def default_command(app):
+    app.log.error('A namespace must be specified use "--help" to see all options')
+    sys.exit(1)
+
+
+def list_command(app):
+    app.log.info('Running list command')
+    app.config.parse_file('../config.json')
 
     settings.init()
 
     # IF AWS IS ENABLED
-    if settings.config['aws']['enabled']:
+    if app.config.get_section_dict('aws')['enabled']:
 
-        logging.info('AWS: Enabled')
+        app.log.info('AWS: Enabled')
 
         ecs_services = []
 
         # IF AWS AUTH TYPE IS PROFILE CALL aws_auth_profile() FUNCTION
-        if settings.config['aws']['auth']['type'] == 'profile':
+        if app.config.get_section_dict('aws')['auth']['type'] == 'profile':
 
-            logging.info('AWS: Using profile authentication')
+            app.log.info('AWS: Using profile authentication')
 
             # FOR EACH PROFILE
-            for aws_profile_name in settings.config['aws']['auth']['profile names']:
+            for aws_profile_name in app.config.get_section_dict('aws')['auth']['profile names']:
 
-                print('AWS: Trying to auth with profile {0}'.format(aws_profile_name))
+                app.log.info('AWS: Trying to auth with profile {0}'.format(aws_profile_name))
 
                 # IF NO REGIONS ARE SPECIFIED FETCH ALL
-                if len(settings.config['aws']['regions']) == 0:
+                if len(app.config.get_section_dict('aws')['regions']) == 0:
 
-                    logging.info('AWS: Regions is empty, fetching all regions')
+                    app.log.info('AWS: Regions is empty, fetching all regions')
 
                     # FOR EACH REGION
                     for ecs_region in aws.list_regions('ecs'):
 
-                        ecs_client = aws.profile_client(aws_profile_name, ecs_region, 'ecs')
+                        ecs_client = aws.profile_client(app, aws_profile_name, ecs_region, 'ecs')
 
-                        aws_services = aws_caller(ecs_client, ecs_services)
+                        aws_services = aws_caller(app, ecs_client, ecs_services)
                 else:
 
                     # FOR EACH REGION
-                    for ecs_region in settings.config['aws']['regions']:
+                    for ecs_region in app.config.get_section_dict('aws')['regions']:
 
-                        ecs_client = aws.profile_client(aws_profile_name, ecs_region, 'ecs')
+                        ecs_client = aws.profile_client(app, aws_profile_name, ecs_region, 'ecs')
 
-                        aws_services = aws_caller(ecs_client, ecs_services)
+                        aws_services = aws_caller(app, ecs_client, ecs_services)
 
-                # IF OUTPUT IS ENABLED
-                if settings.config['output']['enabled']:
-
-                    # IF OUTPUT TYPE IS TABLE
-                    if settings.config['output']['type'] == 'table':
-
-                        table.basic_table(aws_services)
+                output.main(app, aws_services)
 
         # IF AWS AUTH TYPE IS DEFAULT
-        elif settings.config['aws']['auth']['type'] == 'default':
+        elif app.config.get_section_dict('aws')['auth']['type'] == 'default':
 
-            logging.info('AWS: Using default authentication')
+            app.log.info('AWS: Using default authentication')
 
             # IF NO REGIONS ARE SPECIFIED FETCH ALL
-            if len(settings.config['aws']['regions']) == 0:
+            if len(app.config.get_section_dict('aws')['regions']) == 0:
 
-                logging.info('AWS: Regions is empty, fetching all regions')
+                app.log.info('AWS: Regions is empty, fetching all regions')
 
                 # FOR EACH REGION
                 for ecs_region in aws.list_regions('ecs'):
 
                     ecs_client = aws.default_client(ecs_region, 'ecs')
 
-                    aws_services = aws_caller(ecs_client, ecs_services)
+                    aws_services = aws_caller(app, ecs_client, ecs_services)
             else:
 
                 # FOR EACH REGION
-                for ecs_region in settings.config['aws']['regions']:
+                for ecs_region in app.config.get_section_dict('aws')['regions']:
 
                     ecs_client = aws.default_client(ecs_region, 'ecs')
 
-                    aws_services = aws_caller(ecs_client, ecs_services)
+                    aws_services = aws_caller(app, ecs_client, ecs_services)
 
-            # IF OUTPUT IS ENABLED
-            if settings.config['output']['enabled']:
+            output.main(app, aws_services)
 
-                logging.info('OUTPUT: Output enabled')
-
-                # IF OUTPUT TYPE IS TABLE
-                if settings.config['output']['type'] == 'table':
-
-                    logging.info('OUTPUT: Table output enabled')
-
-                    table.basic_table(aws_services)
-                else:
-                    logging.error('OUTPUT: No valid output type selected')
-        else:
-            logging.error('AWS: Could not determine auth type')
+            app.log.info('AWS: Could not determine auth type')
     else:
-        logging.error('MAIN: Nothing is enabled')
+        app.log.error('MAIN: Nothing is enabled')
+
+
+def compare_command(app):
+    app.log.info('Running compare command')
